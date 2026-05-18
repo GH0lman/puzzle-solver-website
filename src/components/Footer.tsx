5e6cee1bf08e4f36aec9960d7b2fc9fd970af443
GH0lman <georgeholmanwork@gmail.com>
2026-05-18 19:18:52 +1200

    Basic Components Setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+export default function Footer({}: Props) {
+  return (
+    <footer>
+      &copy; 2026
+    </footer>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+export default function Header({}: Props) {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  )
+}
